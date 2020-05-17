474dcae4e8409e2a73eaafb1891faa00060e2522
DanSivewright <61431543+DanSivewright@users.noreply.github.com>
2020-05-17 17:08:41 +0200

    Stripe Payment Method

--- Shop.UI/Pages/Checkout/Payment.cshtml.cs
+++ Shop.UI/Pages/Checkout/Payment.cshtml.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Shop.Application.Cart;
+using Stripe;
+using Stripe.Checkout;
 
 namespace Shop.UI.Pages.Checkout
 {
     public class PaymentModel : PageModel
     {
+        public string PublicKey { get; }
+
+        public PaymentModel(IConfiguration config)
+        {
+            PublicKey = config["Stripe:PublicKey"].ToString();
+        }
+
         public IActionResult OnGet()
         {
             var information = new GetCustomerInformation(HttpContext.Session).Do();
@@ -20,6 +30,29 @@ namespace Shop.UI.Pages.Checkout
             }
 
             return Page();
+        }
+
+        public IActionResult OnPost(string stripeEmail, string stripeToken)
+        {
+            var customerOptions = new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            };
+            var customerService = new CustomerService();
+            Customer customer = customerService.Create(customerOptions);
+
+            var chargeOptions = new ChargeCreateOptions
+            {
+                Customer = customer.Id,
+                Description = "Custom t-shirt",
+                Amount = 500,
+                Currency = "usd",
+            };
+            var chargeService = new ChargeService();
+            Charge charge = chargeService.Create(chargeOptions);
+
+            return RedirectToPage("/Index");
         }
     }
 }
